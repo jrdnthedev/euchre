f06fda26e05c09ff0fb6c8ce2b07580a91f544a8
Andre McMorris <ajmcmorris@gmail.com>
2025-08-28 17:31:46 -0400

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:vitest/recommended',
+    'prettier',
+  ],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'vitest'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // Customize your rules here
+    'prettier/prettier': 'error'
+  },
+}
